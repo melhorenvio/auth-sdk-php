8df89a06cdfc314ce605c07988a1e62a5ab57005
Pedro Barros <pdbmachado@inf.ufpel.edu.br>
2021-01-06 15:01:15 -0300

    remoção de dados de teste

--- examples/example2.php
+++ examples/example2.php
@@ -7,9 +7,9 @@ use MelhorEnvio\Auth\OAuth2;
 session_start();
 
 $auth = new OAuth2(
-    624,
-    'ZNlrnALWQf4O2Q1xFWJiwIE004rOXQH3sgKJX86f',
-    'http://teste.sandbox.com.br'
+    ,
+    '',
+    ''
 );
 
 if (isset($_GET['error'])) {
