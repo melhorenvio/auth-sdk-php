97d4d48bfed1b5d8b61f48e0c69b52bf14ab6293
Thyago Santos <thyago.santos@melhorenvio.com>
2022-10-24 14:30:25 -0300

    test: fix composer require in tests

--- tests/Feature/OauthTest.php
+++ tests/Feature/OauthTest.php
@@ -2,7 +2,7 @@
 
 namespace MelhorEnvio\MelhorEnvio\Feature\Unit;
 
-require "vendor/autoload.php";
+require_once __DIR__. '/../../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 use MelhorEnvio\Auth\OAuth2;
