f2e8453d7f6dca2570c0a5500233d2a0f2cf1aa1
Thyago Santos <thyago.santos@melhorenvio.com>
2022-10-25 15:20:10 -0300

    test: add get refresh token tests

--- tests/Unit/OAuthTest.php
+++ tests/Unit/OAuthTest.php
@@ -13,14 +13,20 @@ use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use JsonException;
 use MelhorEnvio\Auth\Exceptions\AccessTokenException;
+use MelhorEnvio\Auth\Exceptions\RefreshTokenException;
 use MelhorEnvio\Auth\OAuth2;
 use MelhorEnvio\Tests\TestCase;
 use Mockery;
 
 class OAuthTest extends TestCase
 {
+    private const TEST_TOKEN_TYPE = '::token-type::';
+    private const TEST_EXPIRES_IN = '::expires-in::';
+    private const TEST_ACCESS_TOKEN = '::access-token::';
     private const TEST_CODE = '::code::';
     private const TEST_STATE = '::state::';
+    private const TEST_REFRESH_TOKEN = '::refresh-token::';
+    private const APPLICATION_X_WWW_FORM_URLENCODED = 'application/x-www-form-urlencoded';
 
     private string $testClientId;
     private string $testClientSecret;
@@ -137,10 +143,10 @@ class OAuthTest extends TestCase
     public function gets_access_token(string $environment, string $url): void
     {
         $expectedResponse = [
-            'token_type' => '::token-type::',
-            'expires_in' => '::expires-in::',
-            'access_token' => '::access-token::',
-            'refresh_token' => '::refresh-token::',
+            'token_type' => self::TEST_TOKEN_TYPE,
+            'expires_in' => self::TEST_EXPIRES_IN,
+            'access_token' => self::TEST_ACCESS_TOKEN,
+            'refresh_token' => self::TEST_REFRESH_TOKEN,
         ];
 
         $expectedBody = http_build_query([
@@ -171,7 +177,7 @@ class OAuthTest extends TestCase
         $this->assertSame($expectedResponse, $sut);
         $this->assertSame("$url/oauth/token", (string)$request->getUri());
         $this->assertSame($expectedBody, (string)$request->getBody());
-        $this->assertSame('application/x-www-form-urlencoded', $request->getHeader('Content-Type')[0]);
+        $this->assertSame(self::APPLICATION_X_WWW_FORM_URLENCODED, $request->getHeader('Content-Type')[0]);
     }
 
     /**
@@ -204,6 +210,85 @@ class OAuthTest extends TestCase
         }
 
         $this->fail(sprintf("%s exception was not thrown.", AccessTokenException::class));
+    }
+
+    /**
+     * @test
+     * @small
+     * @dataProvider environmentProvider
+     * @throws JsonException|AccessTokenException
+     * @throws RefreshTokenException
+     */
+    public function gets_refreshed_token(string $environment, string $url): void
+    {
+        $expectedResponse = [
+            'token_type' => self::TEST_TOKEN_TYPE,
+            'expires_in' => self::TEST_EXPIRES_IN,
+            'access_token' => self::TEST_ACCESS_TOKEN,
+            'redirect_uri' => $this->testRedirectUri,
+            'code' => self::TEST_CODE,
+        ];
+
+        $expectedBody = http_build_query([
+            'grant_type' => 'refresh_token',
+            'client_id' => $this->testClientId,
+            'client_secret' => $this->testClientSecret,
+            'refresh_token' => self::TEST_REFRESH_TOKEN,
+        ]);
+
+        $container = [];
+        $history = Middleware::history($container);
+        $client = $this->createClientMock($expectedResponse, $history);
+
+        $oAuth2 = new OAuth2(
+            $this->testClientId,
+            $this->testClientSecret,
+            $this->testRedirectUri,
+        );
+        $oAuth2->setEnvironment($environment);
+        $oAuth2->setClient($client);
+
+        $sut = $oAuth2->refreshToken(self::TEST_REFRESH_TOKEN);
+
+        /** @var Request $request */
+        $request = $container[0]['request'];
+
+        $this->assertSame($expectedResponse, $sut);
+        $this->assertSame("$url/oauth/token", (string)$request->getUri());
+        $this->assertSame($expectedBody, (string)$request->getBody());
+        $this->assertSame(self::APPLICATION_X_WWW_FORM_URLENCODED, $request->getHeader('Content-Type')[0]);
+    }
+
+    /**
+     * @test
+     * @small
+     * @throws JsonException
+     */
+    public function throws_refresh_token_exception_when_a_client_errors_occurs_while_issuing_an_refresh_token(): void
+    {
+        $expectedResponse = ['foo' => 'bar'];
+        $expectedResponseAsJson = json_encode($expectedResponse, JSON_THROW_ON_ERROR);
+        $expectedStatusCode = 422;
+
+        $client = $this->createMockClientThatThrowsClientException($expectedResponse, $expectedStatusCode);
+
+        $oAuth2 = new OAuth2(
+            $this->testClientId,
+            $this->testClientSecret,
+            $this->testRedirectUri,
+        );
+        $oAuth2->setClient($client);
+
+        try {
+            $oAuth2->refreshToken(self::TEST_REFRESH_TOKEN);
+        } catch (RefreshTokenException $e) {
+            $this->assertSame($expectedResponseAsJson, $e->getMessage());
+            $this->assertSame($expectedStatusCode, $e->getCode());
+
+            return;
+        }
+
+        $this->fail(sprintf("%s exception was not thrown.", RefreshTokenException::class));
     }
 
     public function environmentProvider(): array
