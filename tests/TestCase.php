3d078960bc0d477beaac1703973d4b9828f38fa2
Thyago Santos <thyago.santos@melhorenvio.com>
2022-10-25 18:44:10 -0300

    build: remove useless package vlucas/phpdotenv

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,16 +4,8 @@ namespace MelhorEnvio\Tests;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Dotenv\Dotenv;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $dotenv = Dotenv::createUnsafeImmutable(__DIR__ . '/../');
-        $dotenv->load();
-    }
 }
